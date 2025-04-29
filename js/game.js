f830c6622630fecb52f3be3de7578a8cb9450548
pajekow <77214172+pajekow@users.noreply.github.com>
2025-04-29 19:28:05 +0200

    Poprawki do gry.

--- js/game.js
+++ js/game.js
@@ -203,7 +203,9 @@ function drawShadow() {
 	while (!collisionAt(shadow, 0, 1)) {
 		shadow.y++
 	}
+	ctx.globalAlpha = 0.5 // Ustawienie przezroczystości na 50% (rozjaśnienie)
 	drawFigure(shadow, 'shadow')
+	ctx.globalAlpha = 1.0 // Przywrócenie pełnej przezroczystości dla innych elementów
 }
 
 function collisionAt(figure, dx, dy) {
@@ -263,4 +265,20 @@ function restartGame() {
 function updateUI() {
 	document.getElementById('score').innerText = `Punkty: ${score}`
 	document.getElementById('level').innerText = `Poziom: ${level}`
+}
+
+function lightenColor(color, percent) {
+	const num = parseInt(color.slice(1), 16),
+		amt = Math.round(2.55 * percent),
+		R = (num >> 16) + amt,
+		G = ((num >> 8) & 0x00ff) + amt,
+		B = (num & 0x0000ff) + amt
+	return `#${(
+		0x1000000 +
+		(R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+		(G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
+		(B < 255 ? (B < 1 ? 0 : B) : 255)
+	)
+		.toString(16)
+		.slice(1)}`
 }
